4f20965ff6d0a0b24491bb384265ecd85fb45d47
ZinurX <145262061+ZinurX@users.noreply.github.com>
2024-12-09 17:34:52 +0300

    first one

---
+++ src/main/java/users/com/data/Builders/BuildersApplication.java
@@ -0,0 +1,13 @@
+package users.com.data.Builders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildersApplication.class, args);
+	}
+
+}
