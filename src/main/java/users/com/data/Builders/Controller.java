3eed4126786491372860c8e17e0c46ad1163a43b
ZinurX <145262061+ZinurX@users.noreply.github.com>
2024-12-10 10:22:04 +0300

    add model

--- src/main/java/users/com/data/Builders/Controller.java
+++ src/main/java/users/com/data/Builders/Controller.java
@@ -1,12 +1,15 @@
 package users.com.data.Builders;
 
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
     @GetMapping("/build")
-    public String buildOne (){
-        return "build";
+    public String buildOne (ModelMap model){
+        model.addAttribute("message","hi mvc");
+        return "build <h1>First</h1>";
+
     }
 }
